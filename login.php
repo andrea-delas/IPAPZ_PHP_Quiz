8e38923fa7d712d4da70f81a7b9e5ab68b100a67
Andrea Delaš <andrea.kramar28@gmail.com>
2019-02-20 16:24:00 +0100

    Initial commit

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<?php include('server.php') ?>
+<?php include('functions.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,25 +7,26 @@
 </head>
 <body>
   <div class="header">
-  	<h2>Login</h2>
+    <h2>Login</h2>
   </div>
-	 
   <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
+
+    <?php echo display_error(); ?>
+
+    <div class="input-group">
+      <label>Username</label>
+      <input type="text" name="username" >
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+    <div class="input-group">
+      <button type="submit" class="btn" name="login_btn">Login</button>
+    </div>
+    <p>
+      Not yet a member? <a href="register.php">Sign up</a>
+    </p>
   </form>
 </body>
 </html>
